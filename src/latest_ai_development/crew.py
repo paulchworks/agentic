5bb8a02549f71c22121e6b2118c9d0baba967fa1
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2025-02-12 23:16:20 +0800

    Update crew.py

--- src/latest_ai_development/crew.py
+++ src/latest_ai_development/crew.py
@@ -17,59 +17,60 @@ def print_output(output: TaskOutput):
 
 @CrewBase
 class LatestAiDevelopment():
-	"""LatestAiDevelopment crew"""
+    """LatestAiDevelopment crew"""
 
 	# Learn more about YAML configuration files here:
 	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
 	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
 
 	# If you would like to add tools to your agents, you can learn more about it here:
 	# https://docs.crewai.com/concepts/agents#agent-tools
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
 			config=self.agents_config['researcher'],
 			verbose=True
 		)
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
 			config=self.agents_config['reporting_analyst'],
 			verbose=True
 		)
 
-	# To learn more about structured task outputs, 
+	# To learn more about structured task outputs,
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def research_task(self) -> Task:
-		return Task(
+    @task
+    def research_task(self) -> Task:
+        return Task(
 			config=self.tasks_config['research_task'],
 			callback=print_output,
 		)
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
 			config=self.tasks_config['reporting_task'],
 			output_file='report.md',
 			callback=print_output,
 			human_input=True,
 		)
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the LatestAiDevelopment crew"""
+    @crew
+    def crew(self) -> Crew:
+        """Creates the LatestAiDevelopment crew"""
 		# To learn how to add knowledge sources to your crew, check out the documentation:
 		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
-		return Crew(
+        return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		# process=Process.hierarchical, 
+		# In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
